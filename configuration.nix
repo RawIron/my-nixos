5217e954ed804ad6a236add31a7fb4c49f246c2e
RawIron <manianology@gmail.com>
2025-05-10 12:35:02 -0700

    Clean up a bit and add sway default packages for clarity

--- configuration.nix
+++ configuration.nix
@@ -37,31 +37,39 @@
 
   services.dbus.packages = with pkgs; [ gcr ];
 
-  # enable Sway window manager
-  programs.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true;
-  };
-  programs.waybar.enable = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    grim # screenshot functionality
-    slurp # screenshot functionality
-    wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
-    mako # notification system developed by swaywm maintainer
-  ];
-
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
         command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
-        user = "michael";
       };
     };
   };
+
+  # Enable Sway window manager
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+    extraPackages = with pkgs; [
+      # Packages used in default config
+      brightnessctl
+      foot # termnial emulator
+      grim # screenshot functionality
+      pulseaudio
+      swayidle
+      swaylock
+      wmenu
+      # Packages added
+      slurp # screenshot functionality
+      wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
+      mako # notification system
+    ];
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  ];
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -92,9 +100,6 @@
     isNormalUser = true;
     description = "michael";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      firefox
-    ];
   };
 
   # Allow unfree packages
