fc59f66978f860255ad1abb7fd73021f7520ddd8
RawIron <manianology@gmail.com>
2025-05-17 17:54:39 -0700

    Remove many programs which are already installed by home-manager

--- shell.nix
+++ shell.nix
@@ -5,58 +5,30 @@ in
 
 pkgs.mkShellNoCC {
   packages = with pkgs; [
-    neovim
     helix
-    rclone
-    git
     tig
     diff-so-fancy
-    tmux
     zellij
-    st
     rxvt-unicode-unwrapped
-    vifm
     joshuto
-    poppler_utils
-    lynx
-    chafa
     timg
     glow
-    ffmpeg_6
     ffmpegthumbnailer
-    mpv
-    feh
     epub-thumbnailer
-    rofi
-    zathura
-    calibre
-    rawtherapee
-    signal-desktop
-    protonvpn-gui
-    encfs
     universal-ctags
     cloc
     scc
-    fzf
-    fd
-    ripgrep
-    bat
-    broot
-    tree
     eza
     lsof
     ncdu
     dust
-    jq
     tealdeer
-    fastfetch
     nmon
-    bottom
     htop
     atop
+    btop
     iotop
-    curl
-    wget
+    bottom
   ];
 
 }
