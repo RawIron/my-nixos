c68f9e4c7cb330abe326d7e27636a5e2d8d01be4
RawIron <manianology@gmail.com>
2025-05-20 17:29:19 -0700

    Add colors of i3bar default, support theme name as parameter and if no name was given show all

--- show_color.sh
+++ show_color.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-theme_name="Breeze-Dark"
+declare -A themes
 
-colors="
+themes[Breeze-Dark]="
   26292D
   ED1515
   11D116
@@ -23,11 +23,57 @@ colors="
   EFF0F1
 "
 
-for color in ${colors}; do
-  red="${color:0:2}"
-  green="${color:2:2}"
-  blue="${color:4:2}"
-  printf '\e[38;2;%d;%d;%dm%s\e[0m\n' 0x${red} 0x${green} 0x${blue} \
-    "the ${theme_name} theme colors are shown here"
-done
+themes[i3bar]="
+31363b20
+ffffff
+666666
+4c7899
+285577
+333333
+5f676a
+222222
+888888
+2f343a
+900000
+"
+
+
+show() {
+  local name="$1"; shift
+  local colours="$*"
+
+  for color in ${colours}; do
+    red="${color:0:2}"
+    green="${color:2:2}"
+    blue="${color:4:2}"
+    printf '\e[38;2;%d;%d;%dm%s\e[0m\n' 0x${red} 0x${green} 0x${blue} \
+      "the ${name} theme color ${color} is shown here"
+  done
+  echo
+}
+
+do_all() {
+  for name in "${!themes[@]}"; do
+    echo "  --- " $name " ---"
+    show ${name} ${themes[$name]}
+  done
+}
+
+do_one() {
+  local name="$1"
+
+  if [[ ${themes[$name]} ]]; then
+    echo "  --- " $name " ---"
+    show ${name} ${themes[$name]}
+  fi
+}
+
+
+selected="${1:-"all"}"
+
+if [[ "$selected" == "all" ]]; then
+  do_all
+else
+  do_one "$selected"
+fi
 
