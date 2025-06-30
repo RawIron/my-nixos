3db817041c7523c621cb0ab55be65131188c8d41
RawIron <manianology@gmail.com>
2025-06-30 12:30:45 -0700

    Allow extra opts as parameter

--- deploy_nixos.sh
+++ deploy_nixos.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-sudo rclone sync ~/workspace/my-nixos /etc/nixos --exclude-from rclone-exclude.txt --verbose
+extra_opts="$*"
+
+sudo rclone sync ~/workspace/my-nixos /etc/nixos --exclude-from rclone-exclude.txt --verbose ${extra_opts}
