5af9e8f626b38e34b9d8a93c1addd50d54887a7d
RawIron <manianology@gmail.com>
2025-07-03 18:41:07 -0700

    Move neovim configuration into new folder

--- home/default.nix
+++ home/default.nix
@@ -24,7 +24,6 @@ in
 
   # link the configuration file in current directory
   # to the specified location in home directory
-  home.file.".config/nvim/init.vim".source = ./init.vim;
   home.file.".config/vifm/vifmrc".source = ./vifmrc;
   home.file.".config/vifm/colors/onedark.vifm".source = ./onedark.vifm;
   home.file.".config/vifm/scripts/ffprobe_jq.sh".source = ./ffprobe_jq.sh;
---
+++ home/programs/nvim/default.nix
@@ -0,0 +1,7 @@
+{ ... }:
+
+{
+  # link the configuration file in current directory
+  # to the specified location in home directory
+  home.file.".config/nvim/init.vim".source = ./init.vim;
+}
