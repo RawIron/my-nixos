89c44d9f386c7fe46432aeb55789d8b5e64d3b9a
RawIron <manianology@gmail.com>
2026-01-13 16:54:33 -0800

    New configuration and hardware-configuration after re-install of nixos

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -14,19 +14,11 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/6690eb09-bf2d-42ff-8efb-0f6e3d738f53";
+    { device = "/dev/disk/by-uuid/e5cbce3f-5dcf-4caf-b7fa-4a6669bdaf57";
       fsType = "ext4";
     };
 
   swapDevices = [ ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s25.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
